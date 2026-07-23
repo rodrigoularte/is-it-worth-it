808a8876f7426b3ed820232c01ca26ff90e371ed
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2026-07-23 20:08:48 -0300

    chore: add aria props on ProgressBar

--- src/components/simulation/ProgressBar.tsx
+++ src/components/simulation/ProgressBar.tsx
@@ -13,7 +13,13 @@ export default function ProgressBar({
       <p className="text-primary text-sm sm:text-base">
         Passo {currentStep} de {totalSteps}
       </p>
-      <div className="bg-primary/20 h-2 w-full rounded-full">
+      <div
+        role="progressbar"
+        aria-valuenow={currentStep}
+        aria-valuemin={1}
+        aria-valuemax={totalSteps}
+        aria-label={`Passo ${currentStep} de ${totalSteps}`}
+        className="bg-primary/20 h-2 w-full overflow-hidden rounded-full">
         <div
           className="bg-primary h-full rounded-full transition-all duration-300"
           style={{ width: `${progress}%` }}
